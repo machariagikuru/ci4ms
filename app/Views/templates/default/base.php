f66933cf475638cc90134e13fe9be2ffd75ad207
sirmegicks <macharia.gikuru@gmail.com>
2025-11-16 16:02:17 +0300

    navbar converted into a partial

--- app/Views/templates/default/base.php
+++ app/Views/templates/default/base.php
@@ -20,60 +20,7 @@
 <body class="d-flex flex-column h-100">
     <main class="flex-shrink-0">
         <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container px-5">
-                <a class="navbar-brand" href="<?= base_url() ?>">
-                    <?php if (empty($settings->logo)): ?>
-                        <?= esc($settings->siteName) ?>
-                    <?php else: ?>
-                        <img src="<?= esc($settings->logo) ?>" alt="<?= esc($settings->siteName) ?>" class="img-fluid">
-                    <?php endif; ?>
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <?php menu($menus ?? []); ?>
-                        <li class="nav-item">
-                            <button class="btn btn-outline-secondary border-0 fw-bold" data-bs-toggle="modal" data-bs-target="#searchModal">
-                                <i class="bi bi-search"></i> Search
-                            </button>
-                        </li>
-
-                        <!-- Authentication Buttons -->
-                        <?php
-                        $authLib = new \Modules\Auth\Libraries\AuthLibrary();
-                        if ($authLib->isLoggedIn()):
-                            $user = $authLib->getUser();
-                            $displayName = $user ? esc($user->firstname ?? 'User') : 'User';
-                            $groupId = session('group_id');
-                            ?>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Hello, <?= $displayName ?>
-                                </a>
-                                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
-                                    <?php if ($groupId == 2): ?>
-                                        <li><a class="dropdown-item" href="<?= base_url('my-account') ?>"><i class="bi bi-person"></i> My Account</a></li>
-                                        <li><hr class="dropdown-divider"></li>
-                                    <?php endif; ?>
-                                    <li><a class="dropdown-item" href="<?= base_url('logout') ?>"><i class="bi bi-box-arrow-right"></i> Logout</a></li>
-                                </ul>
-                            </li>
-                        <?php else: ?>
-                            <li class="nav-item">
-                                <a class="nav-link" href="<?= base_url('login') ?>"><i class="bi bi-box-arrow-in-right"></i> Login</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="<?= base_url('register') ?>"><i class="bi bi-person-plus"></i> Register</a>
-                            </li>
-                        <?php endif; ?>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <?= $this->include('templates/default/partials/_navbar') ?>
         <?= $this->renderSection('content') ?>
     </main>
     <!-- Footer-->
